5f9689cb8f1156cb3807b06fbbb93700ab6a17a9
Darren Le <darrenle98@gmail.com>
2019-04-09 21:31:17 -0400

    added animations

---
+++ js/animation.js
@@ -0,0 +1,26 @@
+(()=>{
+
+	title		=		document.querySelector(".animation h3")
+	line		=		document.querySelector(".aniline")
+	paragraph	=		document.querySelector(".animation p")
+
+	function titleAnimation(){
+		let Tline = new TimelineLite()
+
+			Tline.to(line,1.5,{
+			height:"20vw"
+		})
+
+			.to(title,1,{
+			x: "-101%"
+		})
+
+			.to(paragraph,1,{
+			x: "100%"
+		})
+	}
+
+
+	window.addEventListener("load", titleAnimation)
+
+})()
